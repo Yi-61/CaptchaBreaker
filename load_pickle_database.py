b2b654595bc967eb9471c7a729373b3939bf2842
Yi-61 <33105174+Yi-61@users.noreply.github.com>
2017-11-18 22:52:15 -0800

    Add a preprocessing step
    The preprocessing step includes turning images to grayscale and flatten images

--- load_pickle_database.py
+++ load_pickle_database.py
@@ -1,13 +1,25 @@
 import pickle
 import numpy as np
+from skimage import color
 
 def load_images_labels(filepath):
     data = pickle.load(open(filepath, 'rb'));
 
-    images = data[0];
-    print(images.shape)
+    images_read = data[0];
+    images = preprocess(images_read);
+#     print(images.shape)
     labels = data[1];
-    print(images[0].shape)
+#     print(np.nonzero(labels))
+#     labels = np.asarray(labels)
+#     print(images[0].shape)
+    return [images, labels] #images is a 2d array (nSamples,60*40)
 
-
-    return [images, labels]
+def preprocess(images_read):
+    #images is an array (nSamples,60,40,3)
+    [nSamples,height,width,RGB] = images_read.shape
+    grayImages = color.rgb2gray(images_read)
+    #grayImages is an array (nSamples,60,40)
+    flattenedImages = grayImages.reshape(nSamples,height*width)
+    #flattenedImages is a 2d array (nSamples,60*40)
+    return flattenedImages
+    
